b72d11a9e270773b5ca1bb594437c7756f429762
Allen Guan <apguan@gmail.com>
2019-08-21 16:01:54 -0700

    rebuilding prod

--- build/precache-manifest.149c8cc28cb21d9edf0df778b5ffb5c7.js
+++ build/precache-manifest.149c8cc28cb21d9edf0df778b5ffb5c7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bef369992d49021d01e20e6d00f3c842",
+    "revision": "88dc1338f2c87f79cd5899c3a77323a0",
     "url": "/index.html"
   },
   {
-    "revision": "663dba87f6fe9f1ff8ba",
-    "url": "/static/css/main.b17acb7e.chunk.css"
+    "revision": "56838219ca6a647603c9",
+    "url": "/static/css/main.34de6062.chunk.css"
   },
   {
     "revision": "118fa41bb2625b3833aa",
     "url": "/static/js/2.55cb9cab.chunk.js"
   },
   {
-    "revision": "663dba87f6fe9f1ff8ba",
-    "url": "/static/js/main.660a7f52.chunk.js"
+    "revision": "56838219ca6a647603c9",
+    "url": "/static/js/main.8e2e0c64.chunk.js"
   },
   {
     "revision": "e86913e2cc12e1369564",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5ae5163c187f661fee93a4470a61a8b.js"
+  "/precache-manifest.149c8cc28cb21d9edf0df778b5ffb5c7.js"
 );
 
 self.addEventListener('message', (event) => {
